f38bdb7fd3d0a9b01509a1422106d06f9a5518d1
sukjain <sukant.jain@oracle.com>
2020-05-22 18:14:37 +0530

    added graph

--- src/Components/DataQuality/DataQualityReport.js
+++ src/Components/DataQuality/DataQualityReport.js
@@ -19,6 +19,7 @@ import DeleteIcon from '@material-ui/icons/Delete';
 import {green,red,indigo,orange,blue,grey} from '@material-ui/core/colors';
 import Avatar from '@material-ui/core/Avatar';
 import {Bar,Line,Pie,Column} from 'react-chartjs-2';
+//import { LineChart, XAxis, YAxis, Label, ResponsiveContainer } from 'recharts';
 //import CanvasJSReact from 'canvasjs';
 //import CanvasJSReact from './canvasjs.react';
 //import CanvasJS from './CanvasJS'; 
@@ -119,6 +120,12 @@ const useStyles = makeStyles((theme)=>({
 }));
 
 
+function createData(time, amount) {
+    return { time, amount };
+  }
+  
+  
+
 function DataQualityReport(){
 
     const classes = useStyles();
@@ -131,17 +138,84 @@ function DataQualityReport(){
         setRuleStatus({ ...ruleStatus, [event.target.name]: event.target.checked });
     }
 
+    const state = {
+        labels: ['January', 'February', 'March',
+                 'April', 'May','June','July','August','September','October','November','December'],
+        datasets: [
+          {
+            label: 'Record Passed',
+            backgroundColor: 'rgba(75,192,192,1)',
+            borderColor: 'rgba(0,0,0,1)',
+            borderWidth: 1,
+            data: [65, 59, 80, 81, 56,100,34,57,68,90,12]
+          }
+        ]
+      }    
+
+      const state1 = {
+        labels: ['January', 'February', 'March',
+                 'April', 'May','June','July','August','September','October','November','December'],
+        datasets: [
+          {
+            label: 'Record Passed',
+            backgroundColor: 'rgba(75,192,192,1,10)',
+            borderColor: 'rgba(0,0,0,1)',
+            borderWidth: 1,
+            data: [65, 59, 80, 81, 56,100,34,57,68,90,12]
+          }
+        ]
+      }    
+
+
+
     return (<>
         <Paper variant="outlined" >
             <Typography component={'span'} variant={'body2'}>
             <Box textAlign="center" m={1} fontSize="h5.fontSize" style={{fontWeight: 'bold'}}>
                 Data Quality Report
             </Box>
-
             </Typography>
+            <div>
+        <Bar
+        width="20%"
+        height="2%"
+          data={state}
+          options={{
+            title:{
+              display:true,
+              text:'Range Check By Month Created(Last 12 Months)',
+              fontSize:20
+            },
+            legend:{
+              display:true,
+              position:'right',
+              responsive: true, maintainAspectRatio: true
+            }
+            
+          }}
+        />
+      </div>
 
-
-
+      <div>
+        <Bar
+        width="20%"
+        height="2%"
+          data={state1}
+          options={{
+            title:{
+              display:true,
+              text:'Average Rainfall per month',
+              fontSize:20
+            },
+            legend:{
+              display:true,
+              position:'right',
+              responsive: true, maintainAspectRatio: true
+            }
+            
+          }}
+        />
+      </div>
 
 
         </Paper>
